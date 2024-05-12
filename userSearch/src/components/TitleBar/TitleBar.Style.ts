63eef2fbabe52fa440a138b332da2aec9c761eea
jrummuka <jrummuka@student.hive.fi>
2024-05-12 06:24:53 +0300

    Placeholder title, depencies

---
+++ userSearch/src/components/TitleBar/TitleBar.Style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Title__Container = styled.div`
+    background-color: #282c34; // delete
+    margin: 0;
+    position: relative;
+    margin-top: 5%;
+    display: flex;
+    justify-content: center;
+`;
+
+export const Title__Text = styled.h1`
+background-color: #282392; // delete
+position: relative;
+text-align: center;
+font-size: 3em;
+font-family: "Jersey 10", sans-serif;
+font-weight: 400;
+font-style: normal;
+
+&:hover {
+    color: #f00;
+}`;
---
+++ userSearch/src/components/TitleBar/TitleBar.tsx
@@ -0,0 +1,11 @@
+import { Title__Container , Title__Text } from "./TitleBar.Style.ts"
+
+export const TitleBar = () => {
+    return (
+        <Title__Container>
+            <Title__Text>Github people finder</ Title__Text>
+        </Title__Container>
+    );
+};
+
+export default TitleBar;
